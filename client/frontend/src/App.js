2d1c5f2e4a38a9920144239dc5df2552927873fb
GauravGurav-xvii <guravgaurav679@gmail.com>
2024-04-01 21:52:28 +0530

    ki

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Client Side</div>;
+  return <div className="App">Client Side
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p></div>;
 }
 
 export default App;
